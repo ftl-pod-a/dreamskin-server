6213cae9e63f1d12071698f26b0963b25798fd22
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-07-15 10:40:39 -0700

    Set up folder strcuture for backend
    Initialized models, routes and controllers folders

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get('/', (req, res) => {
+    res.send('I see you ...')
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
