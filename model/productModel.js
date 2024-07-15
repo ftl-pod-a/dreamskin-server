a0d0dd875ab41d2c5dcac67cbf064980a8f1dbc4
Maria Perez Chaparro <mariaperez@mariape-ltmt8yz.internal.salesforce.com>
2024-07-15 11:51:46 -0700

    fix file type for productModel

---
+++ model/productModel.js
@@ -0,0 +1,36 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const getAllProducts = async (filter = {}, orderBy = {}) => {
+  return prisma.productgit .findMany({
+    where: filter,
+    orderBy: orderBy,
+  });
+};
+
+const getProductById = async (id) => {
+  return prisma.product.findUnique({ where: { id: parseInt(id) } });
+};
+
+const createProduct = async (productData) => {
+  return prisma.product.create({ data: productData });
+};
+
+const updateProduct = async (id, productData) => {
+  return prisma.product.update({
+    where: { id: parseInt(id) },
+    data: productData,
+  });
+};
+
+const deleteProduct = async (id) => {
+  return prisma.product.delete({ where: { id: parseInt(id) } });
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
