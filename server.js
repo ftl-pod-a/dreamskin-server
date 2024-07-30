47b53945ec6723ddd3c5adfd913b105129a859b0
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-07-30 13:22:06 -0700

    update link name

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 
 
 app.use(cors({
-  origin: process.env.FRONTED_URL, // Adjust as per your frontend URL
+  origin: process.env.FRONTEND_URL, // Adjust as per your frontend URL
 }));
 
 // Routes
