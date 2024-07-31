152ad0218579d8405ec314347bde6a0f40b63e2d
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-07-31 02:04:19 -0700

    add get user by id endpoint

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { register, login, updateUser, deleteUser } = require("../controller/userController");
+const { register, login, getUserById, updateUser, deleteUser } = require("../controller/userController");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/:user_id", getUserById);
 router.put("/:user_id", updateUser); // Update user endpoint
 router.delete("/:user_id", deleteUser); // Delete user endpoint
 
