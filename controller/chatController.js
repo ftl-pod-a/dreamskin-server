40cbeef6e3023a2902445c54e2ce2cf03d9b76c5
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-08-02 00:22:56 -0700

    create endpoint for chatbot

--- controller/chatController.js
+++ controller/chatController.js
@@ -47,6 +47,41 @@ const chatHandler = async (req, res) => {
   }
 };
 
+const chatBotHandler = async (req, res) => {
+  const { prompt, conversationId } = req.body;
+  let refinedPrompt = prompt + "Only answer if previous sentences is related to skincare or skincare products. Reply in less than 2 sentences.";
+  if (!prompt) {
+    return res.status(400).send("Prompt is empty - it is required");
+  }
+
+  try {
+    let messages = [
+      { role: "system", content: "You are a skincare assistant that lists products in json format" },
+    ];
+
+    messages.push({ role: "user", content: refinedPrompt });
+
+    // get the model - WORKING WITH GEMINI API --------------------
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    //get the result from the model using the generateContent and passing in the prompt
+    const result = await model.generateContent(refinedPrompt);
+    const chatResponse = await result.response.text();
+
+    //new conversation id and saave the chat message to DB
+    const newConversationId = conversationId || Date.now().toString();
+    
+    res.json({
+      prompt: prompt,
+      response: chatResponse,
+      conversationId: newConversationId,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+};
+
 module.exports = {
   chatHandler,
+  chatBotHandler
 };
